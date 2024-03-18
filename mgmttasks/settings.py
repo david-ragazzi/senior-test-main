0ca9d8173d4ee8f57fc62ae5938612ee13cde0c4
David Ragazzi <david.ragazzi@gmail.com>
2024-03-18 09:12:48 -0300

    Implemented filtering and ordering

--- mgmttasks/settings.py
+++ mgmttasks/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "tasks.apps.TasksConfig",
     "rest_framework",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.filters import OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Task
 from .serializers import TaskSerializer
@@ -7,4 +9,7 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ["status", "priority", "created_at"]
+    ordering_fields = ["status", "priority", "created_at"]
+    ordering = ["created_at"]
