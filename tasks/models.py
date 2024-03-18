56da76df9b6c1144b79679cc62d053935fd0f6ac
David Ragazzi <david.ragazzi@gmail.com>
2024-03-18 08:57:30 -0300

    Fix and improve Task model
    -Fix parameters in "description" and "created_at" to the appropriated ones.
    -It was converted STATUS tuple to an enum class in order to conform to SOLID principles: use a literal string ("In Progress"/"fazendo") as parameter instead of an enum item will break the code in case of the tuple be changed but the literal string not.

--- tasks/models.py
+++ tasks/models.py
@@ -1,14 +1,18 @@
 from django.db import models
-from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
-class Tasks(models.Model):
-    STATUS = (
-        ('todo', 'To Do'),
-        ('in_progress', 'In Progress'),
-        ('done', 'Done'),
-    )
+
+class Task(models.Model):
+    class Status(models.TextChoices):
+        TODO = "TODO", _("To Do")
+        IN_PROGRESS = "IN_PROGRESS", _("In Progress")
+        DONE = "DONE", _("Done")
+
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    description = models.TextField(null=True)
-    status = models.CharField(max_length=12, choices=STATUS, default='fazendo')
+    description = models.TextField(blank=True, default="")
+    status = models.CharField(max_length=12, choices=Status.choices, default=Status.IN_PROGRESS)
     priority = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add_date=True)
+    created_at = models.DateTimeField(auto_now_add=True)
